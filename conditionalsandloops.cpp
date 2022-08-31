d7446b537641188fd933ae4231c6a448b286d071
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-08-31 22:43:24 +0530

    Add files via upload

---
+++ conditionalsandloops.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int a ;
+    //  cin>>a;
+    // cout<<"value of n is :"<<n<<endl;
+
+    /*if a is positive
+    if(a>0){
+        cout<<"A is Postive"<<endl;
+    }
+    else{
+        cout<<"A is negative"<<endl;
+    }*/
+
+    /*int a , b ;
+    cout<<"Enter the value of a "<<endl;
+    cin>>a;
+    cout<<"Enter the value of b "<<endl;
+    cin>>b;
+
+    if(a>b){
+        cout<<"a is greater than b "<<endl;
+    }
+    if(b>a){
+        cout<<"b is greater than a"<<endl;
+    }*/
+
+    /*int a;
+    cout << "Enter the value of a" << endl;
+    cin >> a;
+    if (a > 0)
+    {
+        cout << "a is +ve" << endl;
+    }
+    else if (a < 0)
+    {
+        cout << "a is negetive";
+    }
+    else
+    {
+        cout << "a is 0 ";
+    }*/
+
+
+    
+
+}
