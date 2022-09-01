4cdeb0f47d5d324c7454ec0191c215a5b41df418
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-09-01 10:58:51 +0530

    Add files via upload

---
+++ sumofevenno.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ; 
+
+int main (){
+    int n ;
+    cout<<"Enter a number : ";
+    cin>>n;
+
+    int i = 2;
+    int sum = 0;
+
+    while(i<=n){
+        if(i %2==0){
+        sum = sum + i;
+        }
+        i = i + 1;
+    }
+
+    cout<<sum;
+
+}
