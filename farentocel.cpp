52b8a8432affb7a8aebba29fe74e5edff55fb9ac
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-09-01 11:06:27 +0530

    Add files via upload

---
+++ farentocel.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std ;
+
+
+int main (){
+    float f ; 
+    cout<<"Enter the farenheit temp : ";
+    cin>>f;
+    
+    float c =  (5.0/9) * (f -32);
+    cout<<"The celcius temp is "<<c ;
+}
