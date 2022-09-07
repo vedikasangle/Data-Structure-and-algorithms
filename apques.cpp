1b273323fff4f2fa7754d08dfef55377da6ff893
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-09-07 22:40:30 +0530

    Add files via upload

---
+++ apques.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std ;
+
+
+int apAnswer(int n ){
+        int ans = 3*n +7 ; 
+        return ans ; 
+
+   
+}
+
+int main(){
+    int n ; 
+    cin>>n; 
+
+    cout<<"The solution of the ap is "<<apAnswer(n);
+
+}
